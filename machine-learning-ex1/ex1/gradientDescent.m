5fe50e2fda4406f8e05d4dda325789925b257318
Ronald Sunarno <ronald@widha.me>
2015-08-17 23:07:58 -0400

    code cleanup. comments

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,20 +16,6 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-    %hypothesis = X * theta;
-    % display(size(hypothesis));
-    % display(size(y));
-    
-    %variances = hypothesis - y; % Add a column of ones to variances
-    % display("size of variances")
-    % display(size(variances));
-    
-    % display("size of variances * X")
-    % display(X' * variances);
-    
-    %theta = theta - alpha / m * X' * variances;
-    % display(size(theta))
     
     theta = theta - ( alpha / m) * (X' * (X * theta - y));
     % ============================================================
